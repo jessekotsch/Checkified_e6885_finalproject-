610d5d2afe8053f101cde23d06a24f373f307802
Md Monirul Islam <monirul@Mds-MacBook-Pro.local>
2022-05-06 21:37:19 -0400

    resolved error

--- Checkified/src/api/index.js
+++ Checkified/src/api/index.js
@@ -1,44 +1,48 @@
-import { contract, verifyContract } from "../contract";
+import VerifyContract from "../contract";
 
-import store from "../store";
+// import store from '../store'
 
-import Big from "bignumber.js";
+// import Big from 'bignumber.js'
 
 export default {
-  async getName() {
-    return await contract.Instance.methods.name().call();
-  },
-  async getSymbol() {
-    return await contract.Instance.methods.symbol().call();
-  },
-  async approve(limit) {
-    const approveAmount = new Big(limit).times("1e18").toString();
-    const response = await contract.Instance.methods
-      .approve(contract.coreAddress, approveAmount)
-      .sendToBlock({
-        from: store.state.dapp.account,
-        amount: new Big("0").toString(),
-      });
+  // async getName() {
+  //     return await contract.Instance.methods.name().call();
+  // },
+  // async getSymbol() {
+  //     return await contract.Instance.methods.symbol().call();
+  // },
+  // async approve(limit) {
+  //     const approveAmount = new Big(limit).times('1e18').toString();
+  //     const response = await contract.Instance.methods.approve(contract.coreAddress, approveAmount).sendToBlock({
+  //         from: store.state.dapp.account,
+  //         amount: new Big('0').toString()
+  //     });
 
-    if (response.success) {
-      console.log("transaction success: ", response);
-    } else {
-      console.log("transaction failed: ", response);
-    }
+  //     if (response.success) {
+  //         console.log('transaction success: ', response);
+  //     } else {
+  //         console.log('transaction failed: ', response);
+  //     }
 
-    return response;
-  },
-
+  //     return response;
+  // }
   async candidateHandler() {
-    //return await verifyContract.verifyInstance.methods.candidateHandler(snn, firstName, lastName, homeAddress, uni).call()
-    await verifyContract.verifyInstance.methods
-      .candidateHandler(
-        123456789,
-        "firstName",
-        "lastName",
-        "homeAddress",
-        "uni"
+    console.log("heree");
+    await VerifyContract.verifyInstance.methods
+      .candidateHandler("1234", "monirul", "islam", "1234STREET", "1234UNI")
+      .call();
+    await VerifyContract.verifyInstance.methods
+      .institutionHandler(
+        "Columbia University",
+        "1234",
+        "Bachelor of Science",
+        "Computer Science",
+        "2020"
       )
       .call();
+    const result = await VerifyContract.verifyInstance.methods
+      .employerHandler("1234")
+      .call();
+    console.log(result);
   },
 };
--- Checkified/src/contract.js
+++ Checkified/src/contract.js
@@ -1,3 +1,27 @@
+// import Mcp from "./mcp";
+
+// const abi = require("./abi.json");
+
+// const McpFunc = new Mcp();
+
+// McpFunc.Contract.setProvider("http://18.182.45.18:8765");
+
+// const tokenAddress = "0x56f12EC4980d7bbD0470BEb3E713e61a33a1141C";
+// const coreAddress = "0x56f12EC4980d7bbD0470BEb3E713e61a33a1141C";
+
+// const Instance = new McpFunc.Contract(
+//     abi,
+//     tokenAddress
+// );
+
+// const Contract = {
+//     tokenAddress,
+//     Instance,
+//     coreAddress
+// };
+
+// export default Contract;
+
 import Mcp from "./mcp";
 
 const abi = require("./abi.json");
@@ -9,6 +33,7 @@ McpFunc.Contract.setProvider("http://18.182.45.18:8765");
 
 const tokenAddress = "0x56f12EC4980d7bbD0470BEb3E713e61a33a1141C";
 const coreAddress = "0x56f12EC4980d7bbD0470BEb3E713e61a33a1141C";
+
 const verifyAddress = "0x789954166f927359f8788201794c8F51A0Ea1E54"; // VERIFICATION CONTRACT
 
 const Instance = new McpFunc.Contract(abi, tokenAddress);
@@ -21,9 +46,11 @@ const Contract = {
 
 const verifyInstance = new McpFunc.Contract(abi_verification, verifyAddress);
 
-const verifyContract = {
+const VerifyContract = {
   verifyInstance,
   verifyAddress,
 };
 
-export default { Contract, verifyContract };
+console.log(Contract);
+
+export default VerifyContract;
